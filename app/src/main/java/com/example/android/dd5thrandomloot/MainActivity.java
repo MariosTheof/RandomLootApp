aacdf0e5ee6b5598a3a307da8e9b28a5b22268c0
Marios Athanasiadis <Athmarios@gmail.com>
2016-03-23 00:24:40 +0200

    Finished the general idea of the layout. Tweaked the java code a little.

--- app/src/main/java/com/example/android/dd5thrandomloot/MainActivity.java
+++ app/src/main/java/com/example/android/dd5thrandomloot/MainActivity.java
@@ -3,11 +3,23 @@ package com.example.android.dd5thrandomloot;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.lang.reflect.Array;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        populatingArray();
+    }
+
+
+
+    private String[] mundaneLoot = new String[3];
+    private void populatingArray(){
+        mundaneLoot[0] = "scrap";
+        mundaneLoot[1] = "vial of antitoxin";
+        mundaneLoot[2] = "worn whetstone";
     }
 }
